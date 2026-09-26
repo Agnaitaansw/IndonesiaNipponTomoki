444b85ff2a835908f24aa55e7a4c0ae4749d2484
Agnaitaa <Agnaitaansw@gmail.com>
2026-09-26 13:16:04 +0700

    new lah

--- api/index.php
+++ api/index.php
@@ -1,3 +1,34 @@
 <?php
 
-require __DIR__.'/../public/index.php';
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// ↓↓↓ TAMBAHKAN BLOK INI ↓↓↓
+$dirs = [
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/logs',
+];
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+// ↑↑↑ SAMPAI SINI ↑↑↑
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
